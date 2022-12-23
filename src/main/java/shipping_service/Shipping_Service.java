8c0fd0c70f6e8600457d85f96fb53d699b8a03e2
RIO489 <oleksander.hamaiunov.it.2021@lpnu.ua>
2022-12-23 15:56:19 +0200

    boot commit

---
+++ src/main/java/shipping_service/Shipping_Service.java
@@ -0,0 +1,11 @@
+package shipping_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shipping_Service {
+    public static void main(final String[] args) {
+        SpringApplication.run(Shipping_Service.class, args);
+    }
+}
