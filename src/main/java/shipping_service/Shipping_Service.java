de94538236dfd58a7c02a0c2ef18fc778643b523
MaximPolapa <n0220po12@gmail.com>
2022-12-29 14:17:22 +0200

    Test DataBase(.xml, Entity)

--- src/main/java/shipping_service/Shipping_Service.java
+++ src/main/java/shipping_service/Shipping_Service.java
@@ -2,8 +2,9 @@ package shipping_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Shipping_Service {
     public static void main(final String[] args) {
         SpringApplication.run(Shipping_Service.class, args);
