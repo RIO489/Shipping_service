b0c1e0cb3ed579efd2ab33675e47b85fed813754
MaximPolapa <n0220po12@gmail.com>
2023-01-07 22:01:10 +0200

    Connect DB, add .yaml, change .xml

--- src/main/java/shipping_service/Shipping_Service.java
+++ src/main/java/shipping_service/Shipping_Service.java
@@ -3,8 +3,9 @@ package shipping_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Shipping_Service {
     public static void main(final String[] args) {
         SpringApplication.run(Shipping_Service.class, args);
--- src/main/java/shipping_service/shipping_test.java
+++ src/main/java/shipping_service/shipping_test.java
@@ -3,13 +3,31 @@ package shipping_service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @RestController
-@RequestMapping("api/test")
+@RequestMapping("api")
 public class shipping_test {
 
     @RequestMapping("/super")
-    String home(){
+    String home()  {
+        String url = "jdbc:mysql://localhost:3306/shop";
+        String name = "root";
+        String password = "polapamax";
+        try(Connection conn = DriverManager.getConnection(url,name,password);) {
+            Statement statement = conn.createStatement();
+
+            statement.executeUpdate("insert into test() value(5) ");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return "Hello world";
     }
+
+
 
 }
