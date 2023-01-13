500916eae23af91b266217eef01a0163cd163708
MaximPolapa <n0220po12@gmail.com>
2023-01-13 19:23:22 +0200

    Product(add @)

--- src/main/java/shipping_service/entity/Product.java
+++ src/main/java/shipping_service/entity/Product.java
@@ -1,11 +1,18 @@
 package shipping_service.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "product")
 public class Product {
@@ -15,66 +22,4 @@ public class Product {
     private String description;
     private BigDecimal price;
     private boolean active;
-
-    public Product(Long id, String name, String description, BigDecimal price, boolean active) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.active = active;
-    }
-    public Product(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", active=" + active +
-                '}';
-    }
 }
