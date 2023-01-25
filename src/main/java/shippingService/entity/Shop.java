2fe17d5e675d3f819a5dd1338ed84c706227385a
Artem Sadovenko <asadovenko7@gmail.com>
2023-01-25 12:49:05 +0200

    Added shop.java

---
+++ src/main/java/shippingService/entity/Shop.java
@@ -0,0 +1,19 @@
+package shippingService.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Shop {
+    @Id
+    private Long id;
+}
