4fc34a1b2e92f74679c86489b4154dab229c3b97
Artem Sadovenko <asadovenko7@gmail.com>
2023-01-26 14:40:10 +0200

    Added Shop entity and DTO

---
+++ src/main/java/dto/ShopDTO.java
@@ -0,0 +1,13 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public class ShopDTO {
+    private Long id;
+    private String shopName;
+    private String address;
+    private long shopOwnerID;
+    private LocalDateTime timeOpen;
+    private LocalDateTime timeClose;
+    private long holidayID;
+}
--- src/main/java/shippingService/entity/Shop.java
+++ src/main/java/shippingService/entity/Shop.java
@@ -3,18 +3,26 @@ package shippingService.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "product")
+@Table(name = "Shop")
 public class Shop {
     @Id
+    @NonNull
     private Long id;
-    private String name;
+    private String shopName;
+    private String address;
+    private long shopOwnerID;
+    private LocalDateTime timeOpen;
+    private LocalDateTime timeClose;
+    private long holidayID;
 }
